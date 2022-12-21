70151849aec55d596d6cc09eedb975f5d9839ea0
Stalise <atis.kozlov@mail.ru>
2022-12-21 22:19:33 +0300

    add svg icons and mapper

---
+++ src/data/svg-icons.ts
@@ -0,0 +1,13 @@
+import svgInfo from 'assest/static/info-icon.svg';
+import svgWarning from 'assest/static/warning-icon.svg';
+import svgError from 'assest/static/error-icon.svg';
+import svgSuccess from 'assest/static/success-icon.svg';
+import svgCross from 'assest/static/cross-icon.svg';
+
+export const svgIcons = {
+  info: svgInfo,
+  warning: svgWarning,
+  error: svgError,
+  success: svgSuccess,
+  cross: svgCross,
+};
